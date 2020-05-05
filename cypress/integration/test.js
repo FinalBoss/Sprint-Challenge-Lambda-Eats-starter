42a31414a71da939ae2fad97471760ffbf4eded3
Brian Clayton <finalb@projectfinalboss.com>
2020-05-04 20:41:30 -0400

    starting test

---
+++ cypress/integration/test.js
@@ -0,0 +1,23 @@
+describe("Test form inputs", function ()  {
+
+it("Visit localhost", function() {
+    cy.visit("http://localhost:3000/");
+});
+
+it("first test", function() {
+
+
+});
+
+it("second test", function() {
+
+
+
+});
+
+it("third test", function() {
+
+
+});
+
+});
